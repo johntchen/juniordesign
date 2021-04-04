1a01f9e7836a05052d4b6eb03d3b8d43fdc08ce9
Joha Kim <johakim9936@gmail.com>
2021-04-04 15:58:46 -0400

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './App.scss';
+import "./App.scss";
+import Home from "./Home/Home";
 
 function App() {
   return (
@@ -6,13 +7,24 @@ function App() {
       <nav className="Navigation">
         <div>Sandia National Labs</div>
         <ul>
-          <li>Link</li>
+          <li>Component</li>
           <li>Link</li>
         </ul>
-        <div className="SearchBar">
-          Search Bar
-        </div>
+        <div className="SearchBar">Search Bar</div>
       </nav>
+      <Home />
+      <footer className="Footer">
+        <div className="Footer--links">
+          <ul>
+            <li>Contacts</li>
+            <li>Documentation</li>
+            <li>FAQs</li>
+          </ul>
+        </div>
+        <div className="Footer--copyright">
+          Copyright @ 2021 Sandia National Labs
+        </div>
+      </footer>
     </div>
   );
 }
