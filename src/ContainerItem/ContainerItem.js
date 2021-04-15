7a70a27b972d8857e83f9bcc7ead83d8f0eba683
Joha Kim <johakim9936@gmail.com>
2021-04-14 22:10:26 -0400

    fix styling issues in ContainerItem

--- src/ContainerItem/ContainerItem.js
+++ src/ContainerItem/ContainerItem.js
@@ -3,31 +3,29 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 
 function ContainerItem(props) {
-
   const redirect = () => {
     console.log(props);
     props.history.push("/container");
   };
 
   return (
-        <div className="ContainerListItem" onClick={redirect}>
-          <div className="ContainerListItem__img"></div>
-          <div className="ContainerListItem__desc">
-            <h3>{props.app}</h3>
-            <br></br>
-            <p>
-              {" "}
-              {props.description}
-            </p>
-            <br></br>
-            <ul className="Heading__tags">
-            <li>tag</li>
-            <li>tag</li>
-            <li>tag</li>
-            <li>tag</li>
-          </ul>
-          </div>
-        </div>
+    <div className="ContainerItem" onClick={redirect}>
+      <div className="ContainerItem__img">
+        <img src="" alt="" className="" />
+      </div>
+      <div className="ContainerItem__desc">
+        <h3>{props.app}</h3>
+        <br></br>
+        <p> {props.description}</p>
+        <br></br>
+        <ul className="ContainerItem__tags">
+          <li>tag</li>
+          <li>tag</li>
+          <li>tag</li>
+          <li>tag</li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
