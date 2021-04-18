04b372fee605ace8b3c954c58ccf4910b61f5d44
Joha Kim <johakim9936@gmail.com>
2021-04-18 06:59:41 -0400

    fix minor formatting issues

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -2,23 +2,20 @@ import "./Container.scss";
 import React from "react";
 import ContainerItem from "../ContainerItem/ContainerItem";
 
-
-
 function Container() {
-
   const [value, setValue] = React.useState("input");
 
   function inputClicked() {
-    setValue('input');
-  };
+    setValue("input");
+  }
 
   function outputClicked() {
-    setValue('output');
-  };
+    setValue("output");
+  }
 
   function workflowClicked() {
-    setValue('workflow');
-  };
+    setValue("workflow");
+  }
 
   return (
     <div className="Container">
@@ -43,66 +40,72 @@ function Container() {
           </ul>
 
           <div className="Heading__tabs">
-            <div className="Heading__tab" onClick={inputClicked}>Input</div>
-            <div className="Heading__tab" onClick={outputClicked}>Output</div>
-            <div className="Heading__tab" onClick={workflowClicked}>Workflow</div>
+            <div className="Heading__tab" onClick={inputClicked}>
+              Input
+            </div>
+            <div className="Heading__tab" onClick={outputClicked}>
+              Output
+            </div>
+            <div className="Heading__tab" onClick={workflowClicked}>
+              Workflow
+            </div>
           </div>
         </div>
       </div>
 
       <div className="Subsection">
-
-        {/* Input */}
-        {value == "input" ? (
-          <ContainerItem
-            app="Evaluation CONUS"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+        <div className="section">
+          {/* Input */}
+          {value == "input" ? (
+            <ContainerItem
+              app="Evaluation CONUS"
+              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
               quibusdam iusto velit, nobis quasi provident."
-          ></ContainerItem>
-        ) : null}
+            ></ContainerItem>
+          ) : null}
 
-        {value == "input" ? (
-          <ContainerItem
-            app="Evaluation Gatlinburg"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          {value == "input" ? (
+            <ContainerItem
+              app="Evaluation Gatlinburg"
+              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
               quibusdam iusto velit, nobis quasi provident."
-          ></ContainerItem>
-        ) : null}
+            ></ContainerItem>
+          ) : null}
 
-        {/* Output */}
-        {value == "output" ? (
-          <ContainerItem
-            app="KKNN Conus Output"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          {/* Output */}
+          {value == "output" ? (
+            <ContainerItem
+              app="KKNN Conus Output"
+              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
               quibusdam iusto velit, nobis quasi provident."
-          ></ContainerItem>
-        ) : null}
+            ></ContainerItem>
+          ) : null}
 
-        {value == "output" ? (
-          <ContainerItem
-            app="KKNN Gatlinburg Output"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          {value == "output" ? (
+            <ContainerItem
+              app="KKNN Gatlinburg Output"
+              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
               quibusdam iusto velit, nobis quasi provident."
-          ></ContainerItem>
-        ) : null}
+            ></ContainerItem>
+          ) : null}
 
-        {/* Workflow */}
-        {value == "workflow" ? (
-          <ContainerItem
-            app="CoreJJ"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          {/* Workflow */}
+          {value == "workflow" ? (
+            <ContainerItem
+              app="CoreJJ"
+              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
               quibusdam iusto velit, nobis quasi provident."
-          ></ContainerItem>
-        ) : null}
+            ></ContainerItem>
+          ) : null}
 
-        {value == "workflow" ? (
-          <ContainerItem
-            app="Dabbers"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+          {value == "workflow" ? (
+            <ContainerItem
+              app="Dabbers"
+              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
               quibusdam iusto velit, nobis quasi provident."
-          ></ContainerItem>
-        ) : null}
-        
+            ></ContainerItem>
+          ) : null}
+        </div>
       </div>
     </div>
   );
