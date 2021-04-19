e1edb703f2ce3259ad1e4b1efeb49b78bacf01c1
Joha Kim <johakim9936@gmail.com>
2021-04-19 11:55:35 -0400

    class name changes

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -18,42 +18,42 @@ function Container() {
   }
 
   return (
-    <div className="Container">
-      <div className="Heading">
+    <div className="container">
+      <div className="heading">
         <div className="section">
-          <h2 className="Heading__title">KKNN Application</h2>
-          <p className="Heading__desc">
+          <h2 className="heading__title">KKNN Application</h2>
+          <p className="heading__desc">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
             repudiandae, voluptatem maxime iure maiores nisi vero cupiditate
             quae tenetur numquam repellat necessitatibus quam excepturi,
             provident tempore aut omnis, incidunt quidem fuga dolor! Cupiditate
             quis necessitatibus voluptatem exercitationem neque obcaecati
-            magnam. Placeat, harum quo! Amet, veritatis laborum
-            repudiandae reiciendis odit. Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. Fuga, nulla.
+            magnam. Placeat, harum quo! Amet, veritatis laborum repudiandae
+            reiciendis odit. Lorem ipsum dolor sit amet consectetur, adipisicing
+            elit. Fuga, nulla.
           </p>
-          <ul className="Heading__tags">
+          <ul className="heading__tags">
             <li>tag1</li>
             <li>tag2</li>
             <li>tag3</li>
             <li>tag4</li>
           </ul>
 
-          <div className="Heading__tabs">
-            <div className="Heading__tab" onClick={inputClicked}>
+          <div className="heading__tabs">
+            <div className="heading__tab" onClick={inputClicked}>
               Input
             </div>
-            <div className="Heading__tab" onClick={outputClicked}>
+            <div className="heading__tab" onClick={outputClicked}>
               Output
             </div>
-            <div className="Heading__tab" onClick={workflowClicked}>
+            <div className="heading__tab" onClick={workflowClicked}>
               Workflow
             </div>
           </div>
         </div>
       </div>
 
-      <div className="Subsection">
+      <div className="subsection">
         <div className="section">
           {/* Input */}
           {value == "input" ? (
